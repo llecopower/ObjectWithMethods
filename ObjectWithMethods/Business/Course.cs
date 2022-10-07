7df411a4b683ed7145e7c7c001222b14e6e3de64
Alex <llecopower@gmail.com>
2022-10-07 10:39:43 -0400

    Add project files.

---
+++ ObjectWithMethods/Business/Course.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectWithMethods.Business
+{
+    public class Course
+    {
+
+        //custom Methods
+        private int studentId;
+
+        public void SetStudentId(int sId)
+        {
+            studentId = sId;
+        }
+
+        public int GetStudentId()
+        {
+            return studentId;
+        }
+
+        private string firstName;
+        public void SetFirstName(string fname)
+        {
+                firstName = fname;
+        }
+
+        public string GetFirstName()
+        {         
+        return firstName;
+        }
+
+
+        private string lastName;
+        public void SetLastName(string lname)
+        {
+            lastName = lname;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        private float midtermGrade;
+
+        public void SetMidtermGrade(float mTermGrade)
+        { 
+        
+            midtermGrade = mTermGrade;
+        
+        }
+
+        public float GetMidtermGrade()
+            {
+            return midtermGrade;
+        
+            }
+
+        private float finaltermGrade;
+
+        public void SetFinalGrade(float fGrade)
+        {
+
+            finaltermGrade = fGrade;
+
+        }
+
+        public float GetFinalGrade()
+        {
+            return finaltermGrade;
+
+        }
+
+        public string GetGradeInfo(Course oGradeRecord)
+        {
+            string infoGrade = oGradeRecord.GetStudentId().ToString() + "\n" +
+                               oGradeRecord.GetFirstName() + "\n" +
+                               oGradeRecord.GetLastName() + "\n" +
+                               oGradeRecord.GetMidtermGrade() + "\n" +
+                               oGradeRecord.GetFinalGrade() + "\n" +
+                               oGradeRecord.GetFinalResult(oGradeRecord);
+            return infoGrade;
+        }
+
+        private int GetFinalResult(Course oGradeRecord)
+        {
+            const float midterm = 0.4f;
+            const float finalexam = 0.6f;
+            int finalResult = 0;
+
+            finalResult = Convert.ToInt32((oGradeRecord.GetMidtermGrade() * midterm) +
+                                         (oGradeRecord.GetFinalGrade() * finalexam));
+
+            return finalResult;            
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ObjectWithMethods/Form1.cs
@@ -0,0 +1,44 @@
+﻿using ObjectWithMethods.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectWithMethods
+{
+    public partial class Form1 : Form
+    {
+        Course oCourse = new Course();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+
+            oCourse.SetStudentId(123456);
+            oCourse.SetFirstName("Kavya");
+            oCourse.SetLastName("Benitez");
+            oCourse.SetMidtermGrade(100.0f);
+            oCourse.SetFinalGrade(20.0f);           
+        }
+           
+
+        private void buttonGet_Click(object sender, EventArgs e)
+        {
+            string display = oCourse.GetGradeInfo(oCourse);
+
+            MessageBox.Show(display, "Student record");
+
+            labelInfo.Text = oCourse.GetGradeInfo(oCourse);
+            
+        }
+    }
+}
